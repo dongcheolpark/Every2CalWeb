808b58959e91b020ca456f2ba1eca4b3e1e4d1e1
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-12-31 17:29:46 +0900

    디자인 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import everyLogo from './images/Everytime_Logo.png'
-import { ChakraProvider,Input, InputGroup, InputLeftAddon } from '@chakra-ui/react'
+import { Button, ChakraProvider,Input, InputGroup, InputLeftAddon } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
@@ -9,14 +9,25 @@ function App() {
     <ChakraProvider>
       <div className="App">
         <header className="App-header">
-          <img src={everyLogo} className="App-logo" alt="logo" />
-          <p className='Content'>
-            에브리타임 테스트
-          </p>
-          <InputGroup className='Content'>
-            <InputLeftAddon children='id'/>
-            <Input type='text' placeholder='공유 URL을 붙여넣으세요' />
-          </InputGroup>
+          <div className='Form'>
+            <img src={everyLogo} className="App-logo" alt="logo" />
+            <p className='Content'>
+              에브리타임 ICS 생성기
+            </p>
+            <InputGroup className='Content'>
+              <InputLeftAddon children='URL'/>
+              <Input type='text' placeholder='공유 URL을 붙여넣으세요' />
+            </InputGroup>
+            <InputGroup className='Content'>
+              <InputLeftAddon children='시작 날짜'/>
+              <Input type='date' placeholder='공유 URL을 붙여넣으세요' />
+            </InputGroup>
+            <InputGroup className='Content'>
+              <InputLeftAddon children='종료 날짜'/>
+              <Input type='date' placeholder='공유 URL을 붙여넣으세요' />
+            </InputGroup>
+            <Button colorScheme='red'>생성!</Button>
+          </div>
         </header>
       </div>
     </ChakraProvider>
